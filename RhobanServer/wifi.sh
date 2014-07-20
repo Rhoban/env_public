d95d198346e1fbdcd2163fe2d0b24d3807a47000
Olivier <rhoban@Mowgly.(none)>
2014-07-21 00:20:39 +0200

    Wifi.sh

--- RhobanServer/wifi.sh
+++ RhobanServer/wifi.sh
@@ -22,6 +22,7 @@ echo "Connecting to $ESSID with $IP..."
 sudo ifconfig wlan0 down ; 
 sleep 1 ; 
 sudo ifconfig wlan0 up ; 
+sudo iwlist wlan0 scan |grep $ESSID ;
 sudo iwconfig wlan0 mode managed ; 
 sudo iwconfig wlan0 key off ; 
 sudo iwconfig wlan0 essid "$ESSID" ; 
