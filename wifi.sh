69368c8a93bc7489c7a7d2632e69fcd65543970f
Gregwar <g.passault@gmail.com>
2016-06-29 09:31:46 +0200

    Leipzig wifi.sh

---
+++ wifi.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./wifi.sh [field-number]"
+    exit
+fi
+
+ESSID="HL-$1"
+HOSTNAME=`hostname`
+
+if [ "$HOSTNAME" == "chewbacca" ]; then
+    IP="192.168.11.101"
+fi
+if [ "$HOSTNAME" == "mowgly" ]; then
+    IP="192.168.11.102"
+fi
+if [ "$HOSTNAME" == "django" ]; then
+    IP="192.168.11.103"
+fi
+if [ "$HOSTNAME" == "tom" ]; then
+    IP="192.168.11.104"
+fi
+
+echo "Connecting to $ESSID with $IP..."
+sudo killall wpa_supplicant ;
+sudo ifconfig wlan0 down ; 
+sleep 1 ; 
+sudo ifconfig wlan0 up ; 
+sudo iwlist wlan0 scan|grep $ESSID ;
+sudo iwconfig wlan0 mode managed essid $ESSID;
+sudo ifconfig wlan0 $IP netmask 255.255.0.0 ;
+echo "done, pinging field router to check it's ok"
+ping -c 5 192.168.0.1
