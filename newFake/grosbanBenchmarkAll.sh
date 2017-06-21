311050843ef9895476ee43ec9a96b8491dc6dd38
Gregwar <g.passault@gmail.com>
2017-06-21 14:58:57 +0200

    fakeReborn -> fake (& delting old fake & newFake)

--- newFake/grosbanBenchmarkAll.sh
+++
@@ -1,4 +0,0 @@
-ln -sf ../tom/tom.urdf sigmaban.urdf
-ln -sf LeipzTom.xml LocalisationTest.xml
-php forBench.php LocalisationTest.xml > ForBench.xml 
-./Benchmarker ForBench.xml Logs/alphaBanCentreDuTerrain/ Logs/alphaBanDrible/ Logs/alphaBanDeplacement/ 
