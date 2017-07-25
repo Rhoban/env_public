3758a35715e0f7fcb1dc0d5e873666bef0ca27a6
Gregwar <g.passault@gmail.com>
2017-07-25 09:03:08 +0200

    Comment in gen strategies

--- fake/genStrategies.sh
+++ fake/genStrategies.sh
@@ -1,2 +1,4 @@
+echo "Generating strategy for Sigmaban"
 ./KickStrategy -j -x ../common/kicks/SigmabanKicks.xml > ../common/kickStrategy_v1.json
+echo "Generating strategy for Sigmaban V2"
 ./KickStrategy -j -x ../common/kicks/SigmabanV2Kicks.xml > ../common/kickStrategy_v2.json
