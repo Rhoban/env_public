9fcc7197f1fdc839ecb679082c3f82632b22b933
medrimonia <medrimonia@gmail.com>
2018-02-06 11:29:28 +0100

    Elementary modifications of config for Arya

--- boot.sh
+++ boot.sh
@@ -3,4 +3,4 @@ ulimit -c unlimited
 ROBOT=`hostname`
 cd $HOME/env/$ROBOT/
 export LD_LIBRARY_PATH=$HOME/catkin_rel/
-nohup $HOME/catkin_rel/RhobanServer > out.log 2>&1 &
+nohup $HOME/catkin_rel/KidSize > out.log 2>&1 &
