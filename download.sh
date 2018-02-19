e869a2bfdb7dc2deb1375ec1a4970b964d9672fd
medrimonia <medrimonia@gmail.com>
2018-02-19 18:58:30 +0100

    Default config for reactivekicker + slowing down kick_classic (unstable with NUC change)

--- download.sh
+++ download.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+remote="10.0.0.1"
+if [ $# -gt 0 ]
+then
+    remote=$1
+fi 
+
 echo "Retrieving robot environment"
-rsync --exclude core -l -r rhoban@10.0.0.1:env/* .
+rsync --exclude core -l -r rhoban@${remote}:env/* .
 
 echo "Checking git status"
 git status
