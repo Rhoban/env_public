978f687ae17c50f92d31e8915f83f9e35591a610
medrimonia <medrimonia@gmail.com>
2018-04-05 06:02:36 +0200

    Updating generate strategies and strategies

--- strategies/generate_strategies.sh
+++ strategies/generate_strategies.sh
@@ -7,14 +7,14 @@ FLAGS=""
 # Specifiying kick model collection file
 FLAGS="${FLAGS} -j ../common/kicks/sigmaban_plus_kicks.json"
 # Specifiying resolution
-FLAGS="${FLAGS} -d 5 -a 0.25"
+FLAGS="${FLAGS} -d 5 -a 0.2"
 # Adding time tolerance
-FLAGS="${FLAGS} -t 5"
+FLAGS="${FLAGS} -t 3"
 # Adding excentric strategy
 FLAGS="${FLAGS} -e"
 
 #Running learning with grass
-${BIN} ${FLAGS} -c tmp_with_grass.csv > tmp_with_grass.json
+${BIN} ${FLAGS} -c tmp_with_grass.csv -w > tmp_with_grass.json
 
 #Running learning against grass
 ${BIN} ${FLAGS} -c tmp_against_grass.csv -o 0 -w > tmp_against_grass.json
