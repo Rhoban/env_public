aa42bc3b7468afc50fe4f367c53957ac192903ae
medrimonia <medrimonia@gmail.com>
2018-04-06 23:19:07 +0200

    Tuning up head values + updating strategy generator

--- strategies/generate_strategies.sh
+++ strategies/generate_strategies.sh
@@ -5,13 +5,14 @@ BIN="${HOME}/rhoban/workspace/devel_release/lib/kid_size/KickStrategy"
 
 FLAGS=""
 # Specifiying kick model collection file
-FLAGS="${FLAGS} -j ../common/kicks/sigmaban_plus_kicks.json"
+FLAGS="${FLAGS} -j sigmaban_plus_kicks.json"
 # Specifiying resolution
-FLAGS="${FLAGS} -d 5 -a 0.2"
+FLAGS="${FLAGS} -d 5 -a 0.5"
 # Adding time tolerance
 FLAGS="${FLAGS} -t 3"
 # Adding excentric strategy
-FLAGS="${FLAGS} -e"
+#FLAGS="${FLAGS} -e"
+FLAGS="${FLAGS} -f left_corridor.json"
 
 #Running learning with grass
 ${BIN} ${FLAGS} -c tmp_with_grass.csv -w > tmp_with_grass.json
