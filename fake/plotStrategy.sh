2ffca62310e8994660c6007643b32260695e2cdd
Gregwar <g.passault@gmail.com>
2019-04-10 12:03:00 +0200

    Generate strategies script

--- fake/plotStrategy.sh
+++ fake/plotStrategy.sh
@@ -5,7 +5,7 @@ if [ $# -lt 1 ]; then
     exit
 fi
 
-./KickStrategy -l $1 -g > /tmp/o
+KickStrategy -l $1 -g > /tmp/o
 mkfifo /tmp/f
 gnuplot < /tmp/f &
 echo -ne "plot '/tmp/o' u 1:2:3 palette w l\nexit" > /tmp/p
