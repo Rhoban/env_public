bf795d6e94912c653ee8933798e02efaa3294d83
medrimonia <medrimonia@gmail.com>
2019-05-16 20:16:42 +0200

    Updating scripts to extract patches from vive

---
+++ fake/extract_vive_patches.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import subprocess
+from prepare import *
+
+def gatherLogs():
+    """
+    Prepare the environment for rhoban kid_size binary. Update symbolic links
+    and analyze metadata from log.
+    """
+    vision_config = subprocess.check_output("basename $(readlink -f vision_config.json)", shell = True).decode("ascii").strip()
+    if (vision_config != "vive_roi_extractor.json"):
+        print("Unexpected link for vision_config.json: '{:}'".format(vision_config))
+        exit(-1)
+    log_name = subprocess.check_output("basename $(readlink -f workingLog)", shell = True).decode("ascii").strip()
+    output_folder = "vive_data/" + log_name
+    tmp_folder = "ground_truth"
+    forbidden_folders = [output_folder, tmp_folder]
+    for folder in forbidden_folders:
+        if os.path.isdir(folder):
+            print("Folder '{:}' should not exist!".format(folder))
+            exit(-1)
+    for folder in forbidden_folders:
+        os.system("mkdir -p " + folder)
+    # TODO: currently KidSize still returns 0 when stopped with ctrl-c
+    if not os.system("./KidSize"):
+        print("Failed to quit KidSize properly")
+        exit(-1)
+    os.system("mv {:}/* {:}".format(tmp_folder, output_folder))
+    os.system("rm -rf " + tmp_folder)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: " + sys.argv[0] + " <pathsToLogs>")
+        exit(-1)
+    for path in sys.argv[1:]:
+        print("Treating '{:}'".format(path))
+        prepareEnv(path)
+        gatherLogs()
--- fake/prepare.py
+++ fake/prepare.py
@@ -4,34 +4,44 @@ import os
 import sys
 import json
 
-if len(sys.argv) != 2:
-    print("Usage: " + sys.argv[0] + " <pathToLog>")
+def prepareEnv(log_path):
+    """
+    Prepare the environment for rhoban kid_size binary. Update symbolic links
+    and analyze metadata from log.
+    """
+    if not os.path.isdir(log_path):
+        print(log_path + " is not a directory, cannot prepare Env")
+        exit(-1)
 
-log_dir = sys.argv[1]
+    os.system("rm -rf workingLog && ln -sf {:} workingLog".format(log_path))
 
-if not os.path.isdir(log_dir):
-    print(log_dir + " is not a directory")
-    exit(-1)
+    metadata = {}
+    with open("workingLog/metadata.json") as json_file:
+        metadata = json.load(json_file)
 
-os.system("rm -rf workingLog && ln -sf {:} workingLog".format(log_dir))
+    robot = metadata["robot"]
 
-metadata = {}
-with open("workingLog/metadata.json") as json_file:
-    metadata = json.load(json_file)
+    robot_elements = { "calibration.json", "sigmaban.urdf" }
 
-robot = metadata["robot"]
+    for e in robot_elements:
+        os.system("ln -sf ../{:}/{:}".format(robot, e))
 
-robot_elements = { "calibration.json", "sigmaban.urdf" }
+    os.system("ln -sf ../strategies/with_grass.json kickStrategy_with_grass.json")
+    os.system("ln -sf ../strategies/against_grass.json kickStrategy_counter_grass.json")
+    os.system("ln -sf ../common/kicks/sigmaban_plus_kicks.json KickModelCollection.json")
 
-for e in robot_elements:
-    os.system("ln -sf ../{:}/{:}".format(robot, e))
+    if "tracker_serial" in metadata:
+        tracker_serial = metadata["tracker_serial"]
+        print ("Setting tracker_serial to " + tracker_serial)
+        cmd = "sed -i 's/tracker_serial.value = .*/tracker_serial.value = {:}/g' rhio/vive/values.conf".format(tracker_serial)
+        os.system(cmd)
+    elif require_tracker:
+        print("Tracker is not available while required for log '{:}'".format(log_path))
+        exit(-1)
 
-os.system("ln -sf ../strategies/with_grass.json kickStrategy_with_grass.json")
-os.system("ln -sf ../strategies/against_grass.json kickStrategy_counter_grass.json")
-os.system("ln -sf ../common/kicks/sigmaban_plus_kicks.json KickModelCollection.json")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: " + sys.argv[0] + " <pathToLog>")
+        exit(-1)
+    prepareEnv(sys.argv[1])
 
-if "tracker_serial" in metadata:
-    tracker_serial = metadata["tracker_serial"]
-    print ("Setting tracker_serial to " + tracker_serial)
-    cmd = "sed -i 's/tracker_serial.value = .*/tracker_serial.value = {:}/g' rhio/vive/values.conf".format(tracker_serial)
-    os.system(cmd)
